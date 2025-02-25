04aad79fb183fbd64d28d8cdfa8f04b40063ce37
leslieluyu <leslie.luyu@gmail.com>
2025-02-25 10:47:59 +0000

    for  bench v1.2

---
+++ helm-charts/chatqna/setenv-1.1.sh
@@ -0,0 +1,7 @@
+ export MODELDIR="/mnt/models"
+ export IMGREPO="opea"
+ export IMGVER="1.1"
+ export NS="benchmark-yulu"
+ export LABEL="node=benchv1.1"
+ export DEMO_IMGVER="1.1"
+
---
+++ helm-charts/chatqna/setenv-1.2.sh
@@ -0,0 +1,7 @@
+ export MODELDIR="/mnt/models"
+ export IMGREPO="opea"
+ export IMGVER="1.2"
+ export NS="benchmark-yulu"
+ export LABEL="bench=v1.2"
+ export DEMO_IMGVER="1.2"
+
